113c8be5282d521c613452d0b89226e59bea25b7
Gustav Vang Klinker <guk001@edu.zealand.dk>
2024-09-24 13:26:46 +0200

    Add project files.

---
+++ ObligatoriskOpgave_TCPJson/JSONObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObligatoriskOpgave_TCPJson
+{
+    public class JSONObject
+    {
+        public string Method {  get; set; }
+        public int[] nums;
+        public JSONObject() { 
+            nums = new int[2];
+        }
+    }
+}
---
+++ ObligatoriskOpgave_TCPJson/Program.cs
@@ -0,0 +1,40 @@
+﻿using ObligatoriskOpgave_TCPJson;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text.Json;
+
+Console.WriteLine("TCP Client");
+TcpListener listener = new TcpListener(IPAddress.Any, 21);
+listener.Start();
+while (true)
+{
+    TcpClient socket = listener.AcceptTcpClient();
+    IPEndPoint ep = socket.Client.RemoteEndPoint as IPEndPoint;
+    Console.WriteLine("Client connected: " + ep.Address);
+    Task.Run(() => HandleClient(socket));
+}
+
+void HandleClient(TcpClient socket)
+{
+    NetworkStream ns = socket.GetStream();
+    StreamReader sr = new StreamReader(ns);
+    StreamWriter sw = new StreamWriter(ns);
+    sw.WriteLine("Welcome");
+    while (socket.Connected)
+    {
+        sw.WriteLine("Pick method: Add, Subtract or Random");
+        sw.WriteLine("ex: {Method: 'Add', nums: [11,6]}");
+        sw.Flush();
+        string message = sr.ReadLine();
+        JSONObject jOb = JsonSerializer.Deserialize<JSONObject>(message);
+        switch (jOb.Method.ToLower())
+        {
+            case "random": sw.WriteLine(RandomNumberGenerator.GetInt32(jOb.nums[0], jOb.nums[1])); break;
+            case "add": sw.WriteLine(jOb.nums[0] + jOb.nums[1]); break;
+            case "subtract": sw.WriteLine(jOb.nums[0] - jOb.nums[1]); break;
+            default: sw.WriteLine("protocol error"); break;
+        }
+        sw.Flush();
+    }
+}
